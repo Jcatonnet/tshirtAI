560b5050376010ed89397ca15283da3fedab5d08
catonnet <julien.catonnet@essec.edu>
2024-01-10 15:52:34 +0100

    create back end server and integrate openai API

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,5 +1,18 @@
-const AIPicker = () => {
-  return <div>AIPicker</div>;
+/* eslint-disable react/prop-types */
+import CustomButton from "./CustomButton";
+
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aiPicker-textarea"
+      />
+    </div>
+  );
 };
 
 export default AIPicker;
